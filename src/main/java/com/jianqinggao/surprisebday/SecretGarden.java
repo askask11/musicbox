8eae604a94969331e605807b727f177b60cad7f4
Jianqing Gao <vip@jianqinggao.com>
2021-02-28 01:56:45 +0800

    Simplify structure

--- src/main/java/com/jianqinggao/surprisebday/SecretGarden.java
+++ src/main/java/com/jianqinggao/surprisebday/SecretGarden.java
@@ -1,10 +1,13 @@
 /*
- * Author: jianqing
+ * Author: Jianqing Gao
  * Date: Nov 4, 2020
- * Description: This document is created for
+ * Description: This document is created for database access.
  */
 package com.jianqinggao.surprisebday;
 
+import cn.hutool.json.JSONUtil;
+import cn.hutool.setting.Setting;
+import com.google.gson.Gson;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,27 +19,29 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * Database Access.
  *
- * @author jianqing
+ * @author Jianqing Gao
  */
 public class SecretGarden implements AutoCloseable
-        {
-    //database
+{
+
     private String dbName;
     private java.sql.Connection dbConn;
-    
+    private final static Setting SETTING = new Setting("db.setting");
+
     public SecretGarden(String dbName, String host, String dbUsername, String dbPassword, boolean useSSL) throws SQLException, ClassNotFoundException
     {
         this.dbName = dbName;
         this.establishConnection(dbName, host, dbUsername, dbPassword, useSSL);
         //stablishConnection(dbName, dbName, dbUsername, dbPassword, true);); 
     }
-    
+
     public static SecretGarden getDefaultInstance() throws SQLException, ClassNotFoundException
     {
-        return new SecretGarden("musicbox", "db.jianqinggao.com:3306", "musicbox", "kZ3JrBVPHEcElUxW", true);
+        return new SecretGarden(SETTING.get("dbName"), SETTING.get("host"), SETTING.get("user"), SETTING.get("pass"), true);
     }
-    
+
     public void establishConnection(String dbName, String host, String dbUsername, String dbPassword, boolean useSSL) throws SQLException, ClassNotFoundException
     {
         //NO this.dbConn = dbConn;
@@ -52,21 +57,19 @@ public class SecretGarden implements AutoCloseable
         //"-u root -p mysql1 -useSSL false"
         this.dbConn = DriverManager.getConnection(connectionURL, properties);
     }
-    
-    
+
     ///////////////////////////
     /////////////MY MUSICS
     /////////////////////////
-    
-    public ArrayList<Music> selectFromMusics()throws SQLException
+    public ArrayList<Music> selectFromMusics() throws SQLException
     {
         Statement s = dbConn.createStatement();
         ResultSet rs = s.executeQuery("SELECT * FROM musics");
         Music m;
         ArrayList<Music> musics = new ArrayList<>();
-        while(rs.next())
+        while (rs.next())
         {
-            m=new Music();
+            m = new Music();
             m.setId(rs.getInt(1));
             m.setName(rs.getString(2));
             m.setArtist(rs.getString(3));
@@ -76,37 +79,37 @@ public class SecretGarden implements AutoCloseable
             m.setTheme(rs.getString(7));
             musics.add(m);
         }
-         
+
         return musics;
     }
-      ///////////////////////////
+
+///////////////////////////
     /////////////FEEDBACK
     /////////////////////////
-    
-   public int insertIntoFeedback(int f)throws SQLException
-   {
-       Statement s = dbConn.createStatement();
-       return s.executeUpdate("INSERT INTO webfeedbk VALUES("+f+")");      
-   }
-     
+    public int insertIntoFeedback(int f) throws SQLException
+    {
+        Statement s = dbConn.createStatement();
+        return s.executeUpdate("INSERT INTO webfeedbk VALUES(" + f + ")");
+    }
+
     ///////////////////////////
     /////////////RECOMMENDATIONS
     /////////////////////////
-    
-    public int insertIntoRecommendations(String title, String email)throws SQLException
+    public int insertIntoRecommendations(String title, String email) throws SQLException
     {
         PreparedStatement ps = dbConn.prepareCall("INSERT INTO recommendation VALUES(?,?)");
         ps.setString(1, title);
         ps.setString(2, email);
         return ps.executeUpdate();
     }
-    
-    
+
     public static void main(String[] args)
     {
-        try(SecretGarden sg = getDefaultInstance())
+        try (SecretGarden sg = getDefaultInstance())
         {
-            System.out.println(sg.selectFromMusics());
+            ArrayList<Music> l = sg.selectFromMusics();
+            System.out.println(JSONUtil.parseArray(l).toStringPretty());
+            //System.out.println(new Gson().toJson(l));
         } catch (SQLException | ClassNotFoundException ex)
         {
             Logger.getLogger(SecretGarden.class.getName()).log(Level.SEVERE, null, ex);
